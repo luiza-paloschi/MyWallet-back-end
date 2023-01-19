873c46351ea14134a86d1c83c492eaed77f68492
luiza-paloschi <luizapaloschi@gmail.com>
2023-01-19 14:48:17 -0300

    feat: fazerLogin do usuário

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
 import joi from 'joi';
 import bcrypt from 'bcrypt';
+import { v4 as uuid } from 'uuid';
 
 dotenv.config();
 
@@ -52,10 +53,46 @@ app.post("/cadastro", async (req, res) => {
 })
 
 app.post("/", async (req, res) => {
+    const {email, password} = req.body
+
+    const LoginSchema = joi.object({
+      email: joi.string().email().required(),
+      password: joi.string().required(),
+    });
+
+    const validation = LoginSchema.validate({email, password}, { abortEarly: false });
   
+  if (validation.error) {
+    const errors = validation.error.details.map((detail) => detail.message);
+    return res.status(422).send(errors);
+  }
 
     try {
-    
+
+      const user = await db.collection('users').findOne({ email });
+      if(!user  || (user && !bcrypt.compareSync(password, user.password))) return res.status(404).send("Usuário ou senha incorretos!")
+      
+      const token = uuid();
+
+      const logged = await db.collection("sessions").findOne({userId: user._id})
+      if (logged) {
+        await db.collection("sessions").updateOne({ name: logged.name }, 
+          {
+              $set: {
+                  token
+              }
+          })
+        return res.send({token, name: user.name})
+      }
+        
+      await db.collection("sessions").insertOne({
+        userId: user._id,
+        token,
+        name: user.name
+      })
+
+      res.send({token, name: user.name})
+  
   } catch (error) {
     console.error(error)
     res.status(500).send('Algo deu errado')
